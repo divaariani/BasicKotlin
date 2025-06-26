fee22cc052dde3e8dc345812d929d4a712d0a60c
divaariani <divaariani@apps.ipb.ac.id>
2025-06-26 16:52:53 +0700

    explicit and intent practice

--- app/src/main/java/com/ict/basickotlin/MainActivity.kt
+++ app/src/main/java/com/ict/basickotlin/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.ict.basickotlin
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val greeting = getString(R.string.welcome)
+        val name = getString(R.string.app_name)
+        val fullText = "$greeting $name"
+
+        val hello = findViewById<TextView>(R.id.hello)
+        hello.text = fullText
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
         }
     }
 }
